89e4ab23b0789f77fac5a51d1b77dafdb82938c5
ljmhk <lk@amp.co>
2013-07-03 13:42:31 +0100

    Data Gather :: added additional command to make the grep more specific for connection iformation (in case of memcached configuration breaking the grep)

--- data-gather/dataset.sh
+++ data-gather/dataset.sh
@@ -2,18 +2,18 @@
     tmpfile_cat="/tmp/dataset_cat.$$"
     tmpfile_prod="/tmp/dataset_prod.$$"
     tmpfile_srch="/tmp/dataset_srch.$$"
-    tmpfile_basic_prod="/tmp/dataset_baseprod.$$"
-    tmpfile_brands="/tmp/dataset_brandid.$$"
+    tmpfile_localxml="/tmp/local.xml.tmp"
     magepath=$1
     if [ -z $1 ];
         then
             echo "Supply path to magento instance"
             exit 1
         fi
-    dbuser=$(grep -m 1 'username' $magepath/app/etc/local.xml | awk -F[ {'print $3'} | awk -F] {'print $1'})
-    dbpass=$(grep -m 1 'password' $magepath/app/etc/local.xml | awk -F[ {'print $3'} | awk -F] {'print $1'})
-    dbname=$(grep -m 1 'dbname' $magepath/app/etc/local.xml | awk -F[ {'print $3'} | awk -F] {'print $1'})
-    dbhost=$(grep -m 1 'host' $magepath/app/etc/local.xml | awk -F[ {'print $3'} | awk -F] {'print $1'})
+    cat $magepath/app/etc/local.xml | sed -n '/\<connection>/,/<\/connection>/p' > $tmpfile_localxml
+    dbuser=$(grep -m 1 'username' $tmpfile_localxml | awk -F[ {'print $3'} | awk -F] {'print $1'})
+    dbpass=$(grep -m 1 'password' $tmpfile_localxml | awk -F[ {'print $3'} | awk -F] {'print $1'})
+    dbname=$(grep -m 1 'dbname' $tmpfile_localxml | awk -F[ {'print $3'} | awk -F] {'print $1'})
+    dbhost=$(grep -m 1 'host' $tmpfile_localxml | awk -F[ {'print $3'} | awk -F] {'print $1'})
     #If no mysql password then no -p flag
     if [ ! -z $dbpass ];
         then
@@ -28,9 +28,3 @@
     echo search_url >> $tmpfile_srch
     mysql --silent -N -h$dbhost -u$dbuser $dbpass $dbname -e "select query_text from catalogsearch_query where num_results < 10;" >> $tmpfile_srch
     mv $tmpfile_srch ./search_url.csv
-    echo product_id,product_url >> $tmpfile_basic_prod
-    mysql --silent -N -h$dbhost -u$dbuser $dbpass $dbname -e "select CONCAT_WS(',', entity_id, url_path) from catalog_product_flat_1 where colour IS NULL AND size IS NULL AND has_options=0;" >> $tmpfile_basic_prod
-    mv $tmpfile_basic_prod ./cart_products.csv
-    echo url_key,brand_id >> $tmpfile_brands
-    mysql --silent -N -h$dbhost -u$dbuser $dbpass $dbname -e "select CONCAT_WS(',', url_key, brand_id) from brands_brand;" >> $tmpfile_brands
-    mv $tmpfile_brands ./brand_ids.csv
